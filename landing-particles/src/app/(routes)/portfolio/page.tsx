81c8b34eb2327fe034b9ad3fed8bb210ac621f3f
abicontreras27 <wacr.27@gmail.com>
2025-01-20 11:34:58 -0500

    eliminando import de portfolio sin usar

--- landing-particles/src/app/(routes)/portfolio/page.tsx
+++ landing-particles/src/app/(routes)/portfolio/page.tsx
@@ -1,8 +1,5 @@
 "use client"
 
-import Image from "next/image";
-import Link from "next/link";
-
 import { dataPortfolio } from "../../../../data";
 import ContainerPage from "@/components/Container";
 import TransitionPage from "@/components/TransitionPage";
